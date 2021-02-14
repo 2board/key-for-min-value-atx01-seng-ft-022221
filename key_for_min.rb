7945ef04e2e64b37c012af1958b8063758dfd063
David Jun <djun301@gmail.com>
2021-02-14 17:10:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,3 +12,14 @@ def key_for_min_value(name_hash)
   end
   small_key
 end
+
+lowest_key = nil 
+lowest_value = nil 
+name_hash.each do |key, value|
+  if lowest_value == nil || value < lowest_value
+    lowest_value = value
+    lowest_key = key
+  end
+end
+lowest_key
+end
