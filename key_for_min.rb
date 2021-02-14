6d42f21e86d58878900c9034c47e9d4ca87c9d31
David Jun <djun301@gmail.com>
2021-02-14 16:26:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   small_key = nil
-  count = nil 
+  count = 0 
   name_hash.each do |key, value|
     if value <= count
+      small_key = key
       count = value
-      key = small_key
     end
   end
   small_key
